86b346606208769d7288da061460ac1fca9b1ae7
lyxiang <liyuxiang@yit.com>
2019-01-30 16:32:44 +0800

    流式实现，注解实现，由spring托管实现

---
+++ src/main/java/com/lyxiang/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.lyxiang.controller;
+
+import com.google.common.collect.ImmutableMap;
+import com.lyxiang.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @author: liyuxiang
+ * @create: 2019-01-29
+ */
+@RestController
+@RequestMapping(value = "/order/")
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @RequestMapping(value = "submitOrder")
+    public Map<Object, Object> submitOrder() {
+        int orderId = orderService.submitOrder();
+        return ImmutableMap.of("orderId", orderId);
+    }
+
+}
---
+++ src/main/java/com/lyxiang/spring/AbstractOrderStatusMachineEngine.java
@@ -0,0 +1,53 @@
+package com.lyxiang.spring;
+
+import com.lyxiang.common.OrderContext;
+import com.lyxiang.common.OrderEvent;
+import com.lyxiang.common.OrderState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.GenericTypeResolver;
+import org.squirrelframework.foundation.fsm.StateMachineBuilderFactory;
+import org.squirrelframework.foundation.fsm.StateMachineConfiguration;
+import org.squirrelframework.foundation.fsm.UntypedStateMachine;
+import org.squirrelframework.foundation.fsm.UntypedStateMachineBuilder;
+
+/**
+ * @author: liyuxiang
+ * @create: 2019-01-21
+ */
+public abstract class AbstractOrderStatusMachineEngine<T extends UntypedStateMachine> implements ApplicationContextAware {
+
+    private static final Logger logger = LoggerFactory.getLogger(AbstractOrderStatusMachineEngine.class);
+
+    private ApplicationContext applicationContext;
+
+    protected UntypedStateMachineBuilder builder;
+
+    public AbstractOrderStatusMachineEngine() {
+        Class<T> genericType = (Class<T>) GenericTypeResolver.resolveTypeArgument(getClass(), AbstractOrderStatusMachineEngine.class);
+        builder = StateMachineBuilderFactory.create(genericType, ApplicationContext.class);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public void fire(OrderEvent orderEvent, OrderContext orderContext) {
+        T stateMachine = builder.newUntypedStateMachine(OrderState.getState(orderContext.orderDTO.getState()),
+                StateMachineConfiguration.create().enableDebugMode(true).enableAutoStart(true),
+                applicationContext);
+        try {
+            stateMachine.fire(orderEvent, orderContext);
+        } catch (Exception e) {
+            logger.error("stateMachine.fire error");
+        }
+    }
+
+
+    protected abstract boolean accept(OrderState orderState);
+
+}
